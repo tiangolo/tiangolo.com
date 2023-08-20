b6495be57e017800ae0b0a37ffd1f614dddf8468
Doğukan Ürker <dogukanurker@icloud.com>
2023-08-21 02:47:21 +0300

    💄 Hover effect added to link icons

--- components/social-link.tsx
+++ components/social-link.tsx
@@ -6,23 +6,23 @@ export default function SocialLink({
   title,
   icon,
 }: {
-  href: string,
-  title: string,
-  icon: IconDefinition
+  href: string;
+  title: string;
+  icon: IconDefinition;
   children?: React.ReactNode;
 }) {
   return (
     <a
-          href={href}
-          target="_blank"
-          rel="noopener"
-          className="text-green-600 hover:underline block h-6 my-4 mx-4"
-        >
-          <FontAwesomeIcon
-            className="h-full"
-            title={title}
-            icon={icon}
-          ></FontAwesomeIcon>
-        </a>
+      href={href}
+      target="_blank"
+      rel="noopener"
+      className="text-green-600 hover:text-green-700 block h-6 my-4 mx-4 duration-150"
+    >
+      <FontAwesomeIcon
+        className="h-full"
+        title={title}
+        icon={icon}
+      ></FontAwesomeIcon>
+    </a>
   );
 }
