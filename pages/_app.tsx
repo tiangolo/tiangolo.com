e62b5578c31449504a1078e4979995b83af033cc
Sebastián Ramírez <tiangolo@gmail.com>
2024-07-25 21:11:08 -0500

    🔥 Remove old files, no longer needed

--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import { AppProps } from "next/app";
-import "tailwindcss/tailwind.css";
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
